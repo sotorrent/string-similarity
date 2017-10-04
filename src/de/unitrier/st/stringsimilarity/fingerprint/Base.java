b6014d3b3ba976bb06f0031da82081c5e7bd0ae8
Sebastian Baltes <research@sbaltes.com>
2017-10-04 14:45:39 +0200

    Fix winnowing algorithm (fingerprintList)
    * select "rightmost" minimal hash value in window
    * remove duplicate hash values (considering position)
    * update methods fingerprintList and completeFingerprintList
    * update test case

--- src/de/unitrier/st/stringsimilarity/fingerprint/Base.java
+++ src/de/unitrier/st/stringsimilarity/fingerprint/Base.java
@@ -1,9 +1,7 @@
 package de.unitrier.st.stringsimilarity.fingerprint;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import javax.annotation.Nonnull;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -33,18 +31,60 @@ public class Base {
      * @return Fingerprint of nGrams (list of selected hash values)
      */
     public static List<Integer> fingerprintList(List<String> nGrams, int windowSize) {
-        // TODO: Also save positions of selected hashes? (see Schleimer03)
+        // TODO: Also return positions of selected hashes? (see Schleimer03)
+        List<NGramHash> nGramHashes = getNGramHashes(nGrams);
         return IntStream
                 .iterate(0, i -> i+1)
-                .limit(nGrams.size()-windowSize)
-                .mapToObj(currentStartPos -> nGrams.subList(currentStartPos, currentStartPos+windowSize-1))
+                .limit(nGramHashes.size()-windowSize)
+                .mapToObj(currentStartPos -> nGramHashes.subList(currentStartPos, currentStartPos+windowSize-1))
                 .map(windowList -> windowList
                         .stream()
-                        .map(String::hashCode)
-                        .min(Integer::compare))
+                        .min(NGramHash::compareTo))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
+                .distinct() // uses equals()
+                .map(n -> n.hashValue)
                 .collect(Collectors.toList());
+    }
+
+    private static List<NGramHash> getNGramHashes(List<String> nGrams) {
+        // save nGram hash values together with their position
+        List<NGramHash> nGramHashValues = new ArrayList<>(nGrams.size());
+        for (int i=0; i<nGrams.size(); i++) {
+            nGramHashValues.add(new NGramHash(nGrams.get(i).hashCode(), i));
+        }
+        return nGramHashValues;
+    }
+
+    public static class NGramHash implements Comparable<NGramHash> {
+        public int hashValue;
+        public int pos;
+
+        public NGramHash(int hash, int pos) {
+            this.hashValue = hash;
+            this.pos = pos;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof NGramHash) {
+                NGramHash other = (NGramHash) obj;
+                return (this.hashValue == other.hashValue && this.pos == other.pos);
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int compareTo(@Nonnull NGramHash o) {
+            if (this.hashValue > o.hashValue) {
+                return 1;
+            } else if (this.hashValue == o.hashValue) {
+                return Integer.compare(this.pos, o.pos);
+            } else {
+                return -1;
+            }
+        }
     }
 
     /**
@@ -55,15 +95,12 @@ public class Base {
      * @param windowSize Winnowing window size
      * @return List of lists with hash values for each window
      */
-    public static List<List<Integer>> completeFingerprintList(List<String> nGrams, int windowSize) {
+    public static List<List<NGramHash>> completeFingerprintList(List<String> nGrams, int windowSize) {
+        List<NGramHash> nGramHashes = getNGramHashes(nGrams);
         return IntStream
                 .iterate(0, i -> i+1)
-                .limit(nGrams.size()-windowSize)
-                .mapToObj(currentStartPos -> nGrams.subList(currentStartPos, currentStartPos+windowSize-1))
-                .map(windowList -> windowList
-                        .stream()
-                        .map(String::hashCode)
-                        .collect(Collectors.toList()))
+                .limit(nGramHashes.size()-windowSize)
+                .mapToObj(currentStartPos -> nGramHashes.subList(currentStartPos, currentStartPos+windowSize-1))
                 .collect(Collectors.toList());
     }
 
