d71f8d3470143e1a8000c584cc8764a0c13f627d
Sebastian Baltes <research@sbaltes.com>
2017-10-25 14:52:06 +0200

    Switch to own exception class because of build warning

--- src/de/unitrier/st/stringsimilarity/util/InputTooShortException.java
+++ src/de/unitrier/st/stringsimilarity/util/InputTooShortException.java
@@ -1,9 +1,7 @@
 package de.unitrier.st.stringsimilarity.util;
 
 public class InputTooShortException extends IllegalArgumentException {
-    public InputTooShortException() {
-
-    }
+    public InputTooShortException() { }
 
     public InputTooShortException(String message) {
         super(message);
