33be02b2eda6f9b8a3acadfcc9764d7c66baf0de
Sebastian Baltes <research@sbaltes.com>
2017-10-26 15:12:00 +0200

    Add new IllegalSimilarityValueException

---
+++ src/de/unitrier/st/stringsimilarity/util/IllegalSimilarityValueException.java
@@ -0,0 +1,9 @@
+package de.unitrier.st.stringsimilarity.util;
+
+public class IllegalSimilarityValueException extends RuntimeException {
+    public IllegalSimilarityValueException() { }
+
+    public IllegalSimilarityValueException(String message) {
+        super(message);
+    }
+}
